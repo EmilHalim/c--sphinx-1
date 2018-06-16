1e6f9c59dc4e1505f2cd8cf7ad5592a079dae8cd
EmilHalim <emil_halimg@yahoo.com>
2018-06-16 07:43:02 +0200

    update1

---
+++ cmm/config.h
@@ -0,0 +1,16 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/* #undef HAVE_STRCASECMP */
+/* #undef HAVE__STRICMP */
+#define HAVE_STRICMP
+/* #undef HAVE_STRCMPI */
+/* #undef HAVE__STRCMPI */
+
+/* #undef HAVE_STRNCASECMP */
+/* #undef HAVE__STRNICMP */
+#define HAVE_STRNICMP
+
+#define HAVE_MBSRTOWCS
+
+#endif
